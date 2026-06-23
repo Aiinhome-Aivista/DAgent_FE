fa67ac15cdeed6013ad0e691f266f70d5d88bcf8
Sonia Khatun <sonia2khatun4@gmail.com>
2026-06-23 10:58:57 +0530

    api 187

--- src/services/api.config.ts
+++ src/services/api.config.ts
@@ -7,10 +7,10 @@ export interface ApiConfig {
 }
 
 export const defaultConfig: ApiConfig = {
-  baseUrl: 'http://122.163.121.176:3019',
+  // baseUrl: 'http://122.163.121.176:3019',
   // baseUrl: 'http://localhost:3019/',
   // baseUrl: 'http://157.173.221.226:3004',
-  // baseUrl: 'http://187.127.163.17:3019',
+  baseUrl: 'http://187.127.163.17:3019',
   timeout: 10000,
 };
 
